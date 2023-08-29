030e7f77889fcbd915856d43780326139aa48fe8
Rasel Sharif <raselsharif9@gmail.com>
2023-08-29 12:22:26 +0600

    added insterval

---
+++ interval.js
@@ -0,0 +1,12 @@
+console.log(1);
+console.log(2);
+let num = 0;
+const intervalID = setInterval(()=>{
+    // console.log(++num);
+}, 500)
+const setTimeoutId = setTimeout(()=>{
+    // console.log("set time out");
+}, 3000)
+console.log(4);
+console.log(5);
+console.log(6);
